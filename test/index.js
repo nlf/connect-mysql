f881fc87e54acaa4f63047da44f3cee8405982f5
Jason Gerfen <jason.gerfen@gmail.com>
2019-02-27 05:10:49 -0700

    V2.2.0 (#44)
    * emergency fix
    
    * Fix for decrypting session data
    
    * v2.2.0: Upgraded cyrpto functions
    
    * 20190226
    
    * Added test harness and adjusted crypto functions
    
    * Fixed decryption of session data issue
    
    * Updated readme
    
    * Migrated lib/connect-mysql.js
    
    * Dependency updates

---
+++ test/index.js
@@ -0,0 +1,40 @@
+var express = require('express'), // express framework
+    session = require('express-session'), // session middleware
+    cookieParser = require('cookie-parser'), // cookie middleware
+    MySQLStore = require('connect-mysql')(session), // mysql session store
+    options = {
+      secret: 'squirrel',
+      config: {
+        user: 'jas-', 
+        password: 'password',
+        database: 'c9' 
+      }
+    },
+    app = express();
+
+app.use(cookieParser());
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: false,
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 24 * 3,
+    expires: 1000 * 60 * 60 * 24 * 3
+  },
+  store: new MySQLStore(options)
+}));
+
+app.get('/', function (req, res) {
+  if (req.session.views) {
+    req.session.views++
+  } else {
+    req.session.views = 1;
+  }
+
+  res.send('Hello world! '+req.session.views);
+});
+ 
+app.listen(3000, 'localhost');
